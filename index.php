91b689a3a10f2b7236c470e8670bb7985c20dd66
Sophia M <sofiajmarinova@gmail.com>
2020-08-31 13:41:57 -0400

    update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php 
+    //comments with 2 forward slashes
+    // within php tag, insert values into HTML
+    echo '<p>Hello!</p>';
+    // end every statement with a semicolon
+    ?> 
+  </body>
+</html>
