99e2eb51b43dcff84895d7f6d8d12b22af66a16e
Sophia M <sofiajmarinova@gmail.com>
2020-08-31 13:52:37 -0400

    add ArrayVariables

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
   </head>
   <body>
     <?php 
-    // define variables with $ sign and datatype
     // 
-    $name = "PHP Store";
+    // 
     $credit = 1000
-    // use period . to concatenate text and variables
+    // 
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h1>";
-    // 
+
+      // php arrays are like Python dictionaries with key-value pairs
+      // example: $ArrayVariable['keyName'] = 'something';
+      $products['Computer']=750;
+      $products['Car']=15000;
+      $products['iPhone']=1000;
+      $products['Toaster']=75;
+
+      // accessing value stored in an array
+      echo "<p>A car costs ".$products['Car']."</p>";
+
     ?> 
   </body>
 </html>
