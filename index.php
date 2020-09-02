9bf3020c6f69fbfd61b10b8811dbbf5853b486f4
Sophia M <sofiajmarinova@gmail.com>
2020-09-01 21:06:54 -0400

    add ConditionalStatements

--- index.php
+++ index.php
@@ -11,20 +11,26 @@
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h1>";
 
-      //
-      $products['Computer']=750;
-      $products['Car']=15000;
-      $products['iPhone']=1000;
-      $products['Toaster']=75;
+    //
+    $products['Computer']=750;
+    $products['Car']=15000;
+    $products['iPhone']=1000;
+    $products['Toaster']=75;
 
-      // 3 Types of Loops
-        // For: set a value and repeat until value is less than another
-        // While: continue looping until something changes
-        // Foreach: keep looping for every item in an array
-      foreach($products as $key => $value){
-        echo "<p>The ".$key." costs ".$value."</p.>";
+    // 
+    foreach($products as $key => $value){
+      echo "<p>The ".$key." costs ".$value."</p.>";
+    }
+
+    // if statements
+
+    echo "<h2>Items you can afford</h2>";
+    
+    foreach($products as $key => $value){
+      if($value <= $credit){
+        echo "<p>".$key."</p>";
       }
-      
+    };
 
     ?> 
   </body>
